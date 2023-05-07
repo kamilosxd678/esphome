c8faa5d0ff6b3e486fb59646ad768da5a8903b08
kamilosxd678 <kamilosxd678@gmail.com>
2023-05-07 20:17:24 +0200

    Update __init__.py

--- esphome/components/esp32_ble_server/__init__.py
+++ esphome/components/esp32_ble_server/__init__.py
@@ -63,7 +63,7 @@ async def to_code(config):
     if "custom_characteristics" in config:
         print(config["custom_characteristics"])
         for x in config["custom_characteristics"]:
-            cg.add(var.add_custom_characteristics(x["characteristic_uuid"], cg.Literal('|'.join())))
+            cg.add(var.add_custom_characteristics(x["characteristic_uuid"], cg.global_ns('|'.join())))
 
     if CORE.using_esp_idf:
         add_idf_sdkconfig_option("CONFIG_BT_ENABLED", True)
